585a0ad33a9b896f65b4026ca50fcdb03901f758
tajzz <tajzz@LAPTOP-8M3VM1OD>
2018-06-23 11:25:03 -0500

    this is my updated Pageobject Model

---
+++ Copy of WordpressDemoPageObject/src/util/BrowserFactory.java
@@ -0,0 +1,36 @@
+package util;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class BrowserFactory {
+	
+	static WebDriver driver;
+	
+	public static WebDriver startBrowser(String browserName, String url){
+		
+		
+		 if(browserName.equalsIgnoreCase("chrome"))
+		{
+			//System.setProperty("webdriver.chrome.driver", "C:/auto/chromedriver.exe");
+				
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\tajzz\\git\\Selenium_pageobject11\\Copy of WordpressDemo\\drivers\\chromedriver.exe");
+			
+			
+			driver = new ChromeDriver();
+		}
+		
+		driver.get(url);
+		
+		return driver;
+	}
+	
+	
+
+}
